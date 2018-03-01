712bc0cba379a22d9adf275dd4fd8a750fb27ef8
Michael Anderson <michanders@msn.com>
2018-03-01 21:34:42 +0000

    automatically backed up by learn

--- components/Board.js
+++ components/Board.js
@@ -5,9 +5,9 @@ export default class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div className='board'>
-        {board.map((field, i) => {
-          return <Field player={field} onClick={onClick.bind(null, i)} key={i} />;
+      <div className = 'board'>
+        {board.map( (field, i) => {
+          return <Field player = {field} onClick = {onClick.bind(null, i)} key = {i} />;
         })}
       </div>
     );
